12e8b2ea0ecde7b3ff426e01a0b7c8ffabbf9b07
Luis Felipe Gómez A <luis.andrade18@hotmail.com>
2023-07-23 21:05:54 -0500

    Created game card presentation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { GameCardComponent } from './components/game-card/game-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    GameCardComponent
   ],
   imports: [
     BrowserModule,
